c54bf64d4acd4f4c343e8d526fe9f5255aee2ee7
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-08 17:58:49 -0300

    package_json

---
+++ Parte_01/11-PackageJSON/app.js
@@ -0,0 +1,17 @@
+//=======================================
+// Criar Package JSON usando 'npm init'
+
+/*
+* npm init
+* ---package name: geralmente nome da pasta principal
+* ---version: versão do package
+* ---description: descrição do projeto, etc.
+* ---entry point: A starter file da aplicação, exemplo 'app.js' 'index.js'
+* ---test command: (ignorou)
+* ---git repository: git url
+* ---keywords: palavras chaves do projeto
+* ---author: autor do projeto, pessoa/empresa
+* ---license: licensa do projeto, exemplo 'MIT' 'ISC'
+* 
+* então com isso cria o package.json com essas configs iniciais.
+*/
