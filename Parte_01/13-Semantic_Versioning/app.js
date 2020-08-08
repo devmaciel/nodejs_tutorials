cc7414ec3b7a7e19e72bf137e4a6b9c07d9c6738
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-08 18:19:23 -0300

    semantic_versioning

---
+++ Parte_01/13-Semantic_Versioning/app.js
@@ -0,0 +1,19 @@
+//=======================================
+// Node Package Manager
+
+//Package JSON aberto... a aula se refere ainda do package.json da aula passada.
+/*
+* EXEMPLO:
+* "dependencies": {
+*     "lodash": "^4.17.11"  //major.minor.patch
+* }
+*
+* Semântica da versão:
+* 
+* Maior: geralmente uma nova versão do software, com muitas diferenças no software
+* Menor: são pequenas funcionalidades na versão geralmente, depreciar as antigas
+* Patch: bug fix, correções de bugs..
+* 
+* ^: exemplo ^4.x.x versão da 4.0.0 pra cima. 
+* ~: exemplo 4.0.x, só da update nos patchs, e não nos minor/major
+*/
