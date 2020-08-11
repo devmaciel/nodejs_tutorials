ebb2646f3cf6547debe8fb1f9c478944d73c933a
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-11 14:27:55 -0300

    submit email

--- Parte_02/controllers/landing.js
+++ Parte_02/controllers/landing.js
@@ -1,4 +1,11 @@
 //Lógica das Routers.. vem da index.js/routes
+
+//Landing Page
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Curso ExpressJS & NodeJS' });
+}
+
+exports.submit_lead = function(req, res, next) {
+  console.log("lead email:", req.body.lead_email);
+  res.redirect('/');
 }
