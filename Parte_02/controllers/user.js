366e3a6394df133ff8b713e871ccb22dd00f1628
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-12 14:16:35 -0300

    dependencias pra user,login

--- Parte_02/controllers/user.js
+++ Parte_02/controllers/user.js
@@ -1,7 +1,20 @@
+//=====================================
+// GEt
 exports.show_login = function(req,res,nex) {
     res.render('user/login', { formData: {}, erros: {} });
 }
 
 exports.show_signup = function(req,res,nex) {
     res.render('user/signup', { formData: {}, erros: {} });
+}
+
+//==================================
+// POST
+// utilizar package do passportjs pra autenticação
+exports.signup = function(req,res,nex) {
+    
+}
+
+exports.login = function(req,res,nex) {
+    
 }
