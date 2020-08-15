1414c11065eacbd36fe33edca9518dde044476ba
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-14 23:58:49 -0300

    req by param, req by querystring

--- Parte_05/src/routes/person.js
+++ Parte_05/src/routes/person.js
@@ -8,8 +8,25 @@ let personRoute = require('./routes/person');
 applicationCache.use(personRoute);
 
 //Rotas
-router.get('/person', (req, res) => {
-    res.send('teste');
+// router.get('/person', (req, res) => {
+//     res.send('teste');
+// });
+
+//Parametros no request object
+// localhost:8000/person/name
+router.get('/person/:name', (req, res) => {
+    res.send(`Request: ${req.params.name}`);
 });
+
+//QueryString, propriedade da query do objeto requisitado
+// localhost:8000/person?name=ALGUMACOISA&param=OUTRACOISa
+router.get('/person', (req, res) => {
+    if(req.query.name){
+        res.send(`Request: ${req.params.name}`);
+    }else{
+        res.send('Request na person');
+    }
+});
+
 
 module.exports = router;
