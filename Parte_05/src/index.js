ee97c10f83405b2ba92a183146f981973d7f7be4
João Pedro Maciel <joomaciiel2@outlook.com>
2020-08-14 23:31:14 -0300

    express e server

--- Parte_05/src/index.js
+++ Parte_05/src/index.js
@@ -1,1 +1,10 @@
-console.log('teste');
+//Requires Dependencies
+let express = require('express');
+let app = express();
+
+//Middlewares
+app.use(express.static('public'));
+
+//Port
+const PORT = process.env.PORT || 8000; //8000 ou 3000 geralmente
+app.listen(PORT, () => console.info(`Server started on ${PORT}`));
